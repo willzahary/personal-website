3fbc967d253fd6c65d92b33e7eb113fa79356284
William Zahary Henderson <karma8wj@gmail.com>
2023-05-14 01:31:41 -0600

    Made it light

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,19 +7,15 @@ export const Landing = () => {
     return (
         <div className="landing">
             <div className="landing-text">
-                <h1>Hi, I'm Will Zahary</h1>
+                <h1>Will Zahary</h1>
                 <p>
-                    I study <a href="/courses" className="inline-link">math and computer science</a> at McGill University. I'm primarily interested in algorithms and algebra.
+                    I study <a href="/courses" className="inline-link">math and computer science</a> at McGill. I'm primarily interested in algorithms and probability.
                 </p>
                 <p>
-                    Right now, I'm one of the course assistants for Honours Algorithms taught by Luc Devroye.
-                </p>
-                <p>
-                    This summer, I'll be interning at <a href="https://databricks.com/" className="inline-link">Databricks</a> in San Francisco.
+                    I currently live in San Francisco. I'll be back in Montréal in August.
                 </p>
                 <Links />
             </div>
-            <img src={logo} className="logo" alt="logo" />
         </div>
     )
 }
