c6a93eddc1bb12252b19ce7952f24fc35da956fd
William Zahary Henderson <karma8wj@gmail.com>
2024-07-31 20:27:42 -0700

    removed some text on landing, but only on mobile

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,7 +8,7 @@ export const Landing = () => {
                 <h1>Will Zahary</h1>
                 <p>I'm a software engineer and a computer science graduate student at McGill University under the supervision of <a target="_blank" rel="noopener noreferrer" href="http://luc.devroye.org">Luc Devroye</a>. My research interests are <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Branching_process">branching processes</a> and <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Distributed_computing">distributed computing</a>.</p> 
                 <p>I'm lecturing <a target="_blank" rel="noopener noreferrer" href="https://www.mcgill.ca/study/2024-2025/courses/comp-251">COMP 251</a> with Giulia Alberini this Fall.</p>
-                <p>I'm originally from <a className='map' target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Edmonton,+AB+T6J">Edmonton</a>, but I live in <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Capitol+Hill,+Seattle,+WA">Seattle</a> right now.</p>
+                <p className="landing-cities">I'm originally from <a className='map' target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Edmonton,+AB+T6J">Edmonton</a>, but I live in <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Capitol+Hill,+Seattle,+WA">Seattle</a> right now.</p>
                 <Links />
             </div>
             <div className="landing-image">
